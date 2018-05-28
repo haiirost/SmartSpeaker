7e80c2595d00a5976a76e52595fea9580d473cfd
haiirost <haiirost@gmail.com>
2018-05-28 22:34:40 +0300

    Update sphinx.php

--- html/sphinx.php
+++ html/sphinx.php
@@ -5,7 +5,7 @@
 
     $path = getcwd();
     chdir('/var/www/sphinx/');
-    exec("rec -r 16k -e signed-integer -b 16 -c 1 /tmp/ps_file.raw </dev/null >/dev/null 2>&1 &");
+    exec("rec -r 16k -e signed-integer -b 16 -c 1 ps_file.raw </dev/null >/dev/null 2>&1 &");
     echo "<p>Rec!";
     sleep(2);
     exec("pkill rec");

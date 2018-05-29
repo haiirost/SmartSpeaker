94b95ce4c83d30f72f66a017d2d15586d1e989e9
haiirost <haiirost@gmail.com>
2018-05-29 17:14:31 +0300

    Update ps_sf.py

--- sphinx/ps_sf.py
+++ sphinx/ps_sf.py
@@ -3,6 +3,8 @@
 from os import environ, path
 import pyaudio
 
+import speech_recognition as sr
+
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
@@ -14,32 +16,15 @@ config.set_string('-lm', 'j.lm')
 config.set_string('-dict', 'j.dic')
 decoder = Decoder(config)
 
-#p = pyaudio.PyAudio()
-#stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
-#stream.start_stream() 
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
 
-#in_speech_bf = False
-#decoder.start_utt()
-#while True:
-#    buf = stream.read(1024)
-#    if buf:
- #       decoder.process_raw(buf, False, False)
-  #      if decoder.get_in_speech() != in_speech_bf:
-   #         in_speech_bf = decoder.get_in_speech()
-    #        if not in_speech_bf:
-     #           decoder.end_utt()
-      #          if decoder.hyp().hypstr == 'JARVIS':
-       #             print (1)
-        #        else:
-         #           print (decoder.hyp().hypstr)
-          #      decoder.start_utt()
-    #else:
-     #   break    
-#decoder.end_utt()
-
-p = pyaudio.PyAudio()
-#stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
-#stream.start_stream() 
+# write audio to a RAW file
+with open("ps_sf.raw", "wb") as f:
+    f.write(audio.get_raw_data()) 
 
 decoder.start_utt()
 stream = open('ps_sf.raw', 'rb')
